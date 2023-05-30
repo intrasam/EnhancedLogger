006e4c84dd9c508a31156ec95c8f43078fcfd99b
54m3l3 <guccionesamuele@gmail.com>
2023-05-30 22:54:00 +0200

    First implementation of an Aspectj Logger

---
+++ src/main/java/com/logger/ColorfulLogger.java
@@ -0,0 +1,10 @@
+package com.logger;
+
+import org.logger.interfaces.Log;
+
+@Log
+public class ColorfulLogger {
+	public static void main(String[] args) {
+		System.out.println("Example log");
+	}
+}
